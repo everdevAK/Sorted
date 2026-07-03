e478fecfb7815500ebe0e2326a559ba55712d3db
Adil <v1@example.com>
2026-07-03 18:44:25 +0400

    delete feature for tasks added, (not synced with data in store)

--- assets/js/components/task/taskActions.js
+++ assets/js/components/task/taskActions.js
@@ -1,4 +1,5 @@
-import { deleteBtns, createCategoryBtns, editTaskBtns } from "../../state/store.js";
+import { deleteBtns, createCategoryBtns, editTaskBtns, store } from "../../state/store.js";
+import { saveStore } from "../../utils/helpers.js";
 import { renderPopup } from "../popup.js";
 
 export function createTaskActions() {
@@ -21,6 +22,15 @@ export function createTaskActions() {
     const deleteBtn = document.createElement("button");
     deleteBtn.classList.add("delete-task");
     deleteBtn.type = "button";
+    deleteBtn.addEventListener("click", (e) => {
+        const taskCategory = e.target.closest(".task-category");
+        const categroyName = taskCategory.querySelector(".task--category-title").textContent.toLowerCase();
+        const taskId = e.target.closest(".task").id;
+        
+        store[categroyName] = store[categroyName].filter(task => task.id !== taskId);
+        saveStore(store);
+        e.target.closest(".task").remove();
+    });
 
     actions.appendChild(editBtn);
     actions.appendChild(deleteBtn);
